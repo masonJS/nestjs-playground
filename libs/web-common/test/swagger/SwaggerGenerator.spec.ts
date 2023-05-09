d45e386b95f297bbdb70d04eab39be4221bf5b1f
minwoo <mwoodev526@gmail.com>
2023-05-09 22:28:05 +0900

    swaggerGenerator 테스트 코드 작성

--- libs/web-common/test/swagger/SwaggerGenerator.spec.ts
+++ libs/web-common/test/swagger/SwaggerGenerator.spec.ts
@@ -1,53 +1,185 @@
 import { SwaggerGenerator } from './SwaggerGenerator';
 
 describe('SwaggerGenerator', () => {
-  it('getter 접근자에 @ApiProperty 데코레이터를 추가한다,', () => {
+  it('ApiProperty 데코레이터가 getter 프로퍼티에 있으면 무시한다.', () => {
     // given
-    const sourceFileText = `
-    class BuyerCreateRequest {
-      id: number;
-      
-      get name(): string | undefined {
-        return 'name';
-      }
-    
-      get email(): string {
-        return 'email';
-      }
-    }
-    `;
-    const swaggerGenerator = new SwaggerGenerator('path', sourceFileText);
+    const content = `
+export class UserResponse {
+  @ApiProperty()
+  get name(): string {
+    return 'name';
+  }
+}`;
+    const generator = new SwaggerGenerator('response.ts', content);
 
     // when
-    const sourceFiles = swaggerGenerator.addApiPropertyToResponse();
+    generator.addApiPropertyToResponse();
 
     // then
-    expect(sourceFiles[0].getText()).toMatchInlineSnapshot(`
+    const text = generator.text()[0];
+    expect(text).toMatchInlineSnapshot(`
       "import { ApiProperty } from "@nestjs/swagger";
 
-          class BuyerCreateRequest {
-            id: number;
-            
-            @ApiProperty()
-              get name(): string | undefined {
-              return 'name';
-            }
-          
-            @ApiProperty()
-              get email(): string {
-              return 'email';
-            }
-          }
-          "
+      export class UserResponse {
+        @ApiProperty()
+        get name(): string {
+          return 'name';
+        }
+      }"
     `);
   });
 
-  it('XXResponse 클래스의 getter 접근자에  @ApiProperty 데코레이터를 추가한다,', () => {
+  it('ApiProperty 데코레이터가 getter 프로퍼티에 있으면 데코레이터를 붙여준다.', () => {
     // given
-    const swaggerGenerator = new SwaggerGenerator('apps/**/*Response.ts');
+    const content = `
+export class UserResponse {
+  get name(): string {
+    return 'name';
+  }
+}`;
+    const generator = new SwaggerGenerator('response.ts', content);
 
     // when
-    swaggerGenerator.addApiPropertyToResponse();
-    swaggerGenerator.save();
+    generator.addApiPropertyToResponse();
+
+    // then
+    const text = generator.text()[0];
+    expect(text).toMatchInlineSnapshot(`
+      "import { ApiProperty } from "@nestjs/swagger";
+
+      export class UserResponse {
+        @ApiProperty({ type: string, required: true   })
+          get name(): string {
+          return 'name';
+        }
+      }"
+    `);
+  });
+
+  it('리턴 타입이 null 또는 undefined인 경우 required 속성값을 false로 반환한다.', () => {
+    // given
+    const content = `
+export class UserResponse {
+  get name(): string | undefined {
+    return 'name';
+  }
+  get email(): string | string[] | null {
+    return 'email';
+  }
+}`;
+    const generator = new SwaggerGenerator('response.ts', content);
+
+    // when
+    generator.addApiPropertyToResponse();
+
+    // then
+    const text = generator.text()[0];
+    expect(text).toMatchInlineSnapshot(`
+      "import { ApiProperty } from "@nestjs/swagger";
+
+      export class UserResponse {
+        @ApiProperty({ type: string | undefined, required: false   })
+          get name(): string | undefined {
+          return 'name';
+        }
+        @ApiProperty({ type: string | string[] | null, required: false   })
+          get email(): string | string[] | null {
+          return 'email';
+        }
+      }"
+    `);
+  });
+
+  it('리턴 타입을 명시하지 않은 경우 타입 추론을 한다', () => {
+    // given
+    const content = `
+export class UserResponse {
+  #prop?: string;
+  
+  get prop() {
+    return this.#prop;
+  }
+}`;
+    const generator = new SwaggerGenerator('response.ts', content);
+
+    // when
+    generator.addApiPropertyToResponse();
+
+    // then
+    const text = generator.text()[0];
+    expect(text).toMatchInlineSnapshot(`
+      "import { ApiProperty } from "@nestjs/swagger";
+
+      export class UserResponse {
+        #prop?: string;
+        
+        @ApiProperty({ type: string | undefined, required: false   })
+          get prop() {
+          return this.#prop;
+        }
+      }"
+    `);
+  });
+
+  it('리턴 타입이 배열인 경우 타입 추론을 한다.', () => {
+    // given
+
+    const content = `
+export class UserResponse {
+  
+  get prop() {
+    return ['a', 'b'];
+  }
+}`;
+    // when
+    const generator = new SwaggerGenerator('response.ts', content);
+
+    // when
+    generator.addApiPropertyToResponse();
+
+    // then
+    const text = generator.text()[0];
+    expect(text).toMatchInlineSnapshot(`
+      "import { ApiProperty } from "@nestjs/swagger";
+
+      export class UserResponse {
+        
+        @ApiProperty({ type: string, required: true , isArray: true  })
+          get prop() {
+          return ['a', 'b'];
+        }
+      }"
+    `);
+  });
+
+  it('리턴 타입이 클래스인 경우 타입 추론을 한다.', () => {
+    // given
+
+    const content = `
+export class UserResponse {
+  
+  get prop(): ClassA {
+    return 'a';
+  }
+}`;
+    // when
+    const generator = new SwaggerGenerator('response.ts', content);
+
+    // when
+    generator.addApiPropertyToResponse();
+
+    // then
+    const text = generator.text()[0];
+    expect(text).toMatchInlineSnapshot(`
+      "import { ApiProperty } from "@nestjs/swagger";
+
+      export class UserResponse {
+        
+        @ApiProperty({ type: ClassA, required: true   })
+          get prop(): ClassA {
+          return 'a';
+        }
+      }"
+    `);
   });
 });
