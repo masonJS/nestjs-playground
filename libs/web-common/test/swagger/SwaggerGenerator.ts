e90b19778782e57a3b1bbd12a80c2b91b1129ec9
minwoo <mwoodev526@gmail.com>
2023-05-09 22:25:10 +0900

    swaggerGenerator > ApiProperty의 속성값 설정

--- libs/web-common/test/swagger/SwaggerGenerator.ts
+++ libs/web-common/test/swagger/SwaggerGenerator.ts
@@ -1,60 +1,85 @@
-import { Project, SourceFile } from 'ts-morph';
+import { GetAccessorDeclaration, Project, SourceFile, Type } from 'ts-morph';
 
 export class SwaggerGenerator {
+  #project: Project;
   #sourceFiles: SourceFile[];
 
-  constructor(path: string, sourceFileText?: any) {
-    const project = new Project({
-      compilerOptions: {
-        strictNullChecks: true,
-      },
+  constructor(path: string | string[], content?: string) {
+    this.#project = new Project({
+      compilerOptions: { strictNullChecks: true },
     });
 
-    if (sourceFileText) {
-      const sourceFile = project.createSourceFile(path, sourceFileText);
-      this.#sourceFiles = [sourceFile];
+    if (content && typeof path === 'string') {
+      this.#sourceFiles = [this.#project.createSourceFile(path, content)];
+
       return;
     }
 
-    this.#sourceFiles = project.addSourceFilesAtPaths(path);
+    this.#sourceFiles = this.#project.addSourceFilesAtPaths(path);
   }
 
-  addApiPropertyToResponse(): SourceFile[] {
+  addApiPropertyToResponse() {
     this.#sourceFiles.forEach((sourceFile) => {
       this.addImportApiProperty(sourceFile);
+      const classes = sourceFile.getClasses();
 
-      sourceFile.getClasses().forEach((classDeclaration) => {
-        const getAccessors = classDeclaration.getGetAccessors();
+      classes.forEach((classDeclaration) => {
+        const getters = classDeclaration.getGetAccessors();
 
-        getAccessors
-          .filter((accessor) => !accessor.getDecorator('ApiProperty'))
-          .forEach((accessor) => {
-            // add @ApiProperty decorator in get Accessor
-            accessor.addDecorator({
-              name: 'ApiProperty',
-              arguments: [],
-            });
-          });
+        getters
+          .filter((getter) => !getter.getDecorator('ApiProperty'))
+          .forEach((getter) => this.addApiProperty(getter));
       });
     });
-
-    return this.#sourceFiles;
   }
 
-  save() {
-    this.#sourceFiles.forEach((sourceFile) => {
-      sourceFile.saveSync();
+  text(): string[] {
+    return this.#sourceFiles.map((sourceFile) => sourceFile.getText());
+  }
+
+  async save() {
+    await this.#project.save();
+  }
+
+  private addApiProperty(getter: GetAccessorDeclaration) {
+    const returnType = getter.getReturnType();
+    const isOptional = this.isOptionalProperty(returnType);
+
+    const isArray = returnType.isArray();
+    const arrayType = returnType.getArrayElementType();
+
+    getter.addDecorator({
+      name: 'ApiProperty',
+      arguments: [
+        `{ type: ${
+          isArray ? arrayType?.getText() : returnType.getText()
+        }, required: ${!isOptional} ${isArray ? ', isArray: true' : ''}  }`,
+      ],
     });
+  }
+
+  private isOptionalProperty(returnType: Type): boolean {
+    if (returnType.isUnion()) {
+      const unionTypes = returnType.getUnionTypes();
+
+      return unionTypes.some(
+        (type) =>
+          type.getText().includes('undefined') ||
+          type.getText().includes('null'),
+      );
+    }
+
+    return returnType.isUndefined() || returnType.isNull();
   }
 
   private addImportApiProperty(sourceFile: SourceFile) {
     const importDeclaration = sourceFile.getImportDeclarations();
 
-    const isImportApiProperty = importDeclaration.some((importDeclaration) => {
-      return importDeclaration
+    const isImportApiProperty = importDeclaration.some((importDeclaration) =>
+      importDeclaration
         .getNamedImports()
-        .some((namedImport) => namedImport.getName() === 'ApiProperty');
-    });
+        .some((namedImport) => namedImport.getName() === 'ApiProperty'),
+    );
 
     if (isImportApiProperty) {
       return;
