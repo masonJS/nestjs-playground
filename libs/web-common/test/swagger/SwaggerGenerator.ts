49f9276ee1ce39f26d34bb7c5d2cc5f2aa2d713c
minwoo <mwoodev526@gmail.com>
2023-05-12 23:56:51 +0900

    request dto 용 swagger 생성

--- libs/web-common/test/swagger/SwaggerGenerator.ts
+++ libs/web-common/test/swagger/SwaggerGenerator.ts
@@ -1,4 +1,10 @@
-import { GetAccessorDeclaration, Project, SourceFile, Type } from 'ts-morph';
+import {
+  GetAccessorDeclaration,
+  Project,
+  PropertyDeclaration,
+  SourceFile,
+  Type,
+} from 'ts-morph';
 
 export class SwaggerGenerator {
   #project: Project;
@@ -18,6 +24,21 @@ export class SwaggerGenerator {
     this.#sourceFiles = this.#project.addSourceFilesAtPaths(path);
   }
 
+  addApiPropertyToRequest() {
+    this.#sourceFiles.forEach((sourceFile) => {
+      this.addImportApiProperty(sourceFile);
+      const classes = sourceFile.getClasses();
+
+      classes.forEach((classDeclaration) => {
+        const properties = classDeclaration.getProperties();
+
+        properties
+          .filter((property) => !property.getDecorator('ApiProperty'))
+          .forEach((property) => this.addApiPropertyToProperty(property));
+      });
+    });
+  }
+
   addApiPropertyToResponse() {
     this.#sourceFiles.forEach((sourceFile) => {
       this.addImportApiProperty(sourceFile);
@@ -28,7 +49,7 @@ export class SwaggerGenerator {
 
         getters
           .filter((getter) => !getter.getDecorator('ApiProperty'))
-          .forEach((getter) => this.addApiProperty(getter));
+          .forEach((getter) => this.addApiPropertyToGetter(getter));
       });
     });
   }
@@ -41,26 +62,100 @@ export class SwaggerGenerator {
     await this.#project.save();
   }
 
-  private addApiProperty(getter: GetAccessorDeclaration) {
-    const returnType = getter.getReturnType();
-    const isOptional = this.isOptionalProperty(returnType);
+  private addApiPropertyToProperty(property: PropertyDeclaration) {
+    const propertyType = property.getType();
 
-    const isArray = returnType.isArray();
-    const arrayType = returnType.getArrayElementType();
+    const propertyOptions = this.getApiPropertyOptions(propertyType);
 
-    getter.addDecorator({
+    property.addDecorator({
       name: 'ApiProperty',
-      arguments: [
-        `{ type: ${
-          isArray ? arrayType?.getText() : returnType.getText()
-        }, required: ${!isOptional} ${isArray ? ', isArray: true' : ''}  }`,
-      ],
+      arguments: propertyOptions.length
+        ? [`{ ${propertyOptions.join(', ')} }`]
+        : [],
     });
   }
 
-  private isOptionalProperty(returnType: Type): boolean {
-    if (returnType.isUnion()) {
-      const unionTypes = returnType.getUnionTypes();
+  private addApiPropertyToGetter(getter: GetAccessorDeclaration) {
+    const returnType = getter.getReturnType();
+
+    const propertyOptions = this.getApiPropertyOptions(returnType);
+
+    getter.addDecorator({
+      name: 'ApiProperty',
+      arguments: propertyOptions.length
+        ? [`{ ${propertyOptions.join(', ')} }`]
+        : [],
+    });
+  }
+
+  private getApiPropertyOptions(returnType: Type) {
+    const type = this.getTypeReferenceAsString(returnType);
+    const isOptional = this.isOptionalProperty(returnType);
+    const isEnum = returnType.isEnum() || returnType.isEnumLiteral();
+
+    return [
+      type ? `type: ${type}` : undefined,
+      isOptional ? `required: ${!isOptional}` : undefined,
+      isEnum
+        ? `enum: ${this.getImportNameType(returnType.getText())}`
+        : undefined,
+    ].filter(Boolean);
+  }
+
+  private getTypeReferenceAsString(type: Type): string | undefined {
+    if (type.isArray()) {
+      const arrayType = type.getArrayElementType();
+
+      if (!arrayType) {
+        return undefined;
+      }
+      const elementType = this.getTypeReferenceAsString(arrayType);
+
+      return `[${elementType}]`;
+    }
+
+    if (type.isBoolean()) {
+      return Boolean.name;
+    }
+
+    if (type.isNumber()) {
+      return Number.name;
+    }
+
+    if (type.isEnum() || type.isEnumLiteral()) {
+      return undefined;
+    }
+
+    if (type.isString() || type.isStringLiteral()) {
+      return String.name;
+    }
+
+    if (type.isUnion()) {
+      const unionTypes = type.getUnionTypes();
+
+      return this.getTypeReferenceAsString(unionTypes[unionTypes.length - 1]);
+    }
+
+    if (type.isClass()) {
+      return this.getImportNameType(type.getText());
+    }
+
+    const text = type.getText();
+
+    if (text === Date.name) {
+      return text;
+    }
+
+    if (text === 'any' || text === 'unknown' || text === 'object') {
+      return 'Object';
+    }
+
+    return undefined;
+  }
+
+  private isOptionalProperty(type: Type): boolean {
+    if (type.isUnion()) {
+      const unionTypes = type.getUnionTypes();
 
       return unionTypes.some(
         (type) =>
@@ -69,17 +164,26 @@ export class SwaggerGenerator {
       );
     }
 
-    return returnType.isUndefined() || returnType.isNull();
+    return type.isUndefined() || type.isNull();
+  }
+
+  // import('...').ClassA 형태에서 named import인 ClassA 만 가져오도록 한다.
+  private getImportNameType(textType: string) {
+    if (!textType.includes('import(')) {
+      return textType;
+    }
+
+    return textType.split('.')[1];
   }
 
   private addImportApiProperty(sourceFile: SourceFile) {
-    const importDeclaration = sourceFile.getImportDeclarations();
-
-    const isImportApiProperty = importDeclaration.some((importDeclaration) =>
-      importDeclaration
-        .getNamedImports()
-        .some((namedImport) => namedImport.getName() === 'ApiProperty'),
-    );
+    const isImportApiProperty = sourceFile
+      .getImportDeclarations()
+      .some((importDeclaration) =>
+        importDeclaration
+          .getNamedImports()
+          .some((namedImport) => namedImport.getName() === 'ApiProperty'),
+      );
 
     if (isImportApiProperty) {
       return;
