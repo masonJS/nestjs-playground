3393c93bae04136e78f474973c9cc59b59294665
minwoo <mwoodev526@gmail.com>
2023-05-31 23:22:47 +0900

    Swagger Decorator enum 생성

---
+++ libs/web-common/test/swagger/swagger-decorator/NestjsSwaggerDecorator.ts
@@ -0,0 +1,6 @@
+export enum NestjsSwaggerDecorator {
+  API_PROPERTY = 'ApiProperty',
+  API_RESPONSE = 'ApiResponse',
+  API_OPERATION = 'ApiOperation',
+  API_HIDE_PROPERTY = 'ApiHideProperty',
+}
---
+++ libs/web-common/test/swagger/swagger-decorator/SwaggerDecorator.ts
@@ -0,0 +1,5 @@
+export enum SwaggerDecorator {
+  API_OK_RESPONSE_BY = 'ApiOkResponseBy',
+  API_OK_ARRAY_RESPONSE_BY = 'ApiOkArrayResponseBy',
+  API_PAGINATE_RESPONSE = 'ApiPaginateResponse',
+}
