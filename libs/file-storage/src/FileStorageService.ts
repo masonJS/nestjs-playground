62eec62f015d51a888a8ec4f8c709ea97c8c41f4
minwoo <mwoodev526@gmail.com>
2023-07-18 23:43:20 +0900

    S3 File Download 구현

--- libs/file-storage/src/FileStorageService.ts
+++ libs/file-storage/src/FileStorageService.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import {
+  GetObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from '@aws-sdk/client-s3';
 import { Logger } from '@app/logger/Logger';
 import { S3UploadFileRequest } from '@app/file-storage/dto/S3UploadFileRequest';
 import { S3UploadFileResponse } from '@app/file-storage/dto/S3UploadFileResponse';
+import { S3Error } from '@app/file-storage/error/S3Error';
+import { S3DownloadFileResponse } from '@app/file-storage/dto/S3DownloadFileResponse';
 
 @Injectable()
 export class FileStorageService {
@@ -22,8 +28,37 @@ export class FileStorageService {
 
       await this.putFileToS3(command);
     } catch (e) {
-      this.logger.error(`s3 file upload error `, e);
+      this.logger.error(
+        `s3 file upload error: input = ${JSON.stringify(input)}, message = ${
+          e.message
+        }`,
+        e,
+      );
       throw e;
+    }
+  }
+
+  async download(
+    bucket: string,
+    path: string,
+  ): Promise<S3DownloadFileResponse> {
+    const command = new GetObjectCommand({
+      Bucket: bucket,
+      Key: path,
+    });
+
+    try {
+      return await this.getFileToS3(command);
+    } catch (e) {
+      this.logger.error(
+        `s3 file download error: bucket&path = ${JSON.stringify({
+          bucket,
+          path,
+        })} message = ${e.message}`,
+        e,
+      );
+
+      throw new S3Error(e.message);
     }
   }
 
@@ -34,7 +69,20 @@ export class FileStorageService {
 
     if (response.isNotOK) {
       throw new Error(`s3 file upload error: message = ${response.message}`);
-      throw new Error(`s3 file upload error: message = ${response.message}`);
     }
+  }
+
+  private async getFileToS3(
+    command: GetObjectCommand,
+  ): Promise<S3DownloadFileResponse> {
+    const response = await this.s3Client
+      .send(command)
+      .then((output) => new S3DownloadFileResponse(output));
+
+    if (response.isNotOK || !response.body) {
+      throw new Error(`s3 file download error: message = ${response.message}`);
+    }
+
+    return response;
   }
 }
