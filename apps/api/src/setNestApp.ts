e6f0c24daea01c00245861950ef4ec70cc80be1b
minwoo <mwoodev526@gmail.com>
2023-09-25 18:49:10 +0900

    DomainExceptionFilter 구현

--- apps/api/src/setNestApp.ts
+++ apps/api/src/setNestApp.ts
@@ -5,9 +5,16 @@ import {
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { VersioningType } from '@nestjs/common/enums/version-type.enum';
+import { Logger } from '@app/logger/Logger';
+import { GlobalExceptionFilter } from '../../../libs/web-common/src/filter/GlobalExceptionFilter';
+import { DomainExceptionFilter } from '../../../libs/web-common/src/filter/DomainExceptionFilter';
 
 export function setNestApp<T extends INestApplication>(app: T): void {
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalFilters(
+    new GlobalExceptionFilter(app.get(Logger)),
+    new DomainExceptionFilter(app.get(Logger)),
+  );
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix('api');
   app.enableVersioning({
---
+++ libs/web-common/src/filter/DomainExceptionFilter.ts
@@ -0,0 +1,53 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpStatus,
+} from '@nestjs/common';
+import { DomainException } from '../res/exception/DomainException';
+import { Logger } from '@app/logger/Logger';
+import { Request, Response } from 'express';
+import { isDefined } from '../util/isDefined';
+import { ResponseEntity } from '../res/ResponseEntity';
+import { ResponseStatus } from '../res/ResponseStatus';
+import { instanceToPlain } from 'class-transformer';
+
+@Catch(DomainException)
+export class DomainExceptionFilter implements ExceptionFilter {
+  constructor(private readonly logger: Logger) {}
+
+  catch(exception: DomainException, host: ArgumentsHost): any {
+    const request = host.switchToHttp().getRequest<Request>();
+    const response = host.switchToHttp().getRequest<Response>();
+
+    this.logger.info(this.getErrorLogMessage(request, exception), exception);
+
+    response
+      .status(exception.statusCode)
+      .json(
+        instanceToPlain(
+          ResponseEntity.ERROR_WITH(
+            exception.responseMessage,
+            HttpStatus[exception.statusCode] as ResponseStatus,
+          ),
+        ),
+      );
+  }
+
+  private getErrorLogMessage(request: Request, exception: DomainException) {
+    return [
+      `DomainException: message = ${exception.message} path=${request.url}`,
+      exception.parameter
+        ? `parameter = ${JSON.stringify(exception.parameter)}`
+        : null,
+      Object.keys(request.body).length > 0
+        ? `body = ${JSON.stringify(request.body)}`
+        : null,
+      Object.keys(request.query).length > 0
+        ? `query = ${JSON.stringify(request.query)}`
+        : null,
+    ]
+      .filter(isDefined)
+      .join(' ');
+  }
+}
