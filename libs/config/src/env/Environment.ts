f10a977c5e6e77230d318975de997b811058d384
minwoo <mwoodev526@gmail.com>
2023-10-15 19:21:01 +0900

    chatGPT 환경 구성 파일 세팅

--- libs/config/src/env/Environment.ts
+++ libs/config/src/env/Environment.ts
@@ -3,6 +3,7 @@ import { DatabaseEnvironment } from './DatabaseEnvironment';
 import { Type } from 'class-transformer';
 import { SnsEnvironment } from '@app/config/env/SnsEnvironment';
 import { S3Environment } from '@app/config/env/S3Environment';
+import { ChatGPTEnvironment } from '@app/config/env/ChatGPTEnvironment';
 
 export class Environment {
   @ValidateNested()
@@ -19,4 +20,9 @@ export class Environment {
   @IsNotEmpty()
   @Type(() => S3Environment)
   s3: S3Environment;
+
+  @ValidateNested()
+  @IsNotEmpty()
+  @Type(() => ChatGPTEnvironment)
+  chatGPT: ChatGPTEnvironment;
 }
