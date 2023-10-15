7c339fcd142a70623b5069d00aaf5c2061976f55
minwoo <mwoodev526@gmail.com>
2023-10-15 19:34:08 +0900

    openai 라이브러리 설치 및 주입

--- libs/chat-gpt/src/ChatGPTModule.ts
+++ libs/chat-gpt/src/ChatGPTModule.ts
@@ -1,1 +1,26 @@
+import { Module } from '@nestjs/common';
+import { OpenAI } from 'openai';
+import { ConfigService } from '@nestjs/config';
+import { Environment } from '@app/config/env/Environment';
+import { ChatGPTService } from '@app/chat-gpt/ChatGPTService';
+
+@Module({
+  providers: [
+    ChatGPTService,
+    {
+      provide: OpenAI,
+      useFactory: (configService: ConfigService<Environment>) => {
+        const openAi = configService.get('chatGPT', {
+          infer: true,
+        });
+
+        return new OpenAI({
+          apiKey: openAi?.apiKey,
+        });
+      },
+      inject: [ConfigService],
+    },
+  ],
+  exports: [ChatGPTService],
+})
 export class ChatGPTModule {}
