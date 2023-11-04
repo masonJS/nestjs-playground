8228abc1962ad6a19d010a46e7aaa5cdb827b91b
minwoo <mwoodev526@gmail.com>
2023-11-05 00:29:31 +0900

    updateAccess 서비스 구현

--- apps/api/src/buyer/BuyerService.ts
+++ apps/api/src/buyer/BuyerService.ts
@@ -4,9 +4,23 @@ import { BuyerRepository } from './BuyerRepository';
 
 @Injectable()
 export class BuyerService {
+  protected readonly LIMIT_ACCESS_COUNT = 2;
+
   constructor(private readonly buyerRepository: BuyerRepository) {}
 
   async create(request: BuyerCreateRequest) {
     await this.buyerRepository.create(request.toEntity());
+  }
+
+  async updateAccess(id: number) {
+    const accessCount = await this.buyerRepository.updateAndReturnAccessCount(
+      id,
+    );
+
+    if (accessCount > this.LIMIT_ACCESS_COUNT) {
+      throw new Error('Access limit exceeded');
+    }
+
+    return accessCount;
   }
 }
