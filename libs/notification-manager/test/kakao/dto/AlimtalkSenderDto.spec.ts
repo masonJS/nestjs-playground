adf532dae9d1513803bae48cdd2db198f2d01bbe
minwoo <mwoodev526@gmail.com>
2023-12-10 23:59:25 +0900

    [main] kakao AlimtalkSenderDto test 구현

---
+++ libs/notification-manager/test/kakao/dto/AlimtalkSenderDto.spec.ts
@@ -0,0 +1,52 @@
+import { AlimtalkSenderDto } from '@app/notification-manager/kakao/dto/AlimtalkSenderDto';
+import { AlimtalkRecipient } from '@app/notification-manager/kakao/dto/AlimtalkRecipient';
+
+describe('AlimtalkSenderDto', () => {
+  it('수신자가 0명이면 빈배열을 반환한다.', () => {
+    // given
+    const dto = AlimtalkSenderDto.of('code', []);
+
+    // when
+    const result = dto.toRequestBody;
+
+    // then
+    expect(result).toBeEmpty();
+  });
+
+  it('수신자가 1000명이면 한개의 배열을 반환한다.', () => {
+    // given
+    const dto = AlimtalkSenderDto.of(
+      'code',
+      Array.from(
+        { length: 1000 },
+        () => new AlimtalkRecipient('phoneNumber', {}),
+      ),
+    );
+
+    // when
+    const result = dto.toRequestBody;
+
+    // then
+    expect(result).toHaveLength(1);
+    expect(result[0].recipientList).toHaveLength(1000);
+  });
+
+  it('수신자가 1999명이면 한개의 배열을 반환한다.', () => {
+    // given
+    const dto = AlimtalkSenderDto.of(
+      'code',
+      Array.from(
+        { length: 1999 },
+        () => new AlimtalkRecipient('phoneNumber', {}),
+      ),
+    );
+
+    // when
+    const result = dto.toRequestBody;
+
+    // then
+    expect(result).toHaveLength(2);
+    expect(result[0].recipientList).toHaveLength(1000);
+    expect(result[1].recipientList).toHaveLength(999);
+  });
+});
