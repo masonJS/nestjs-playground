3c05703dfaaf121796c365a61fc4565ceff1c39c
masonJS <mwoodev526@gmail.com>
2026-03-20 09:51:13 +0900

    Jest에서 Vitest로 의존성 및 설정 교체

---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+import swc from 'unplugin-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    include: ['**/*.spec.ts'],
+    root: '.',
+    globals: true,
+    environment: 'node',
+    setupFiles: ['./vitest.setup.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+      reportsDirectory: './coverage',
+    },
+  },
+  plugins: [swc.vite(), tsconfigPaths()],
+});
