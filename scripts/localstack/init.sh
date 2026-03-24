89284754e63f4308e0c950d2081dd35c10269438
masonJS <mwoodev526@gmail.com>
2026-03-24 18:23:57 +0900

    LocalStack v4 업그레이드 및 SES 의존성 추가

--- scripts/localstack/init.sh
+++ scripts/localstack/init.sh
@@ -1,6 +1,29 @@
-#!/bin/zsh
+#!/bin/bash
 
 echo "init localstack"
-awslocal sns create-topic --name test-topic --region=ap-northeast-2
+
+REGION="ap-northeast-2"
+
+# SNS
+awslocal sns create-topic --name test-topic --region $REGION
+
+# S3
 awslocal s3 mb s3://test-bucket
+
+# SQS
+awslocal sqs create-queue --queue-name test-queue --region $REGION
+
+# SNS → SQS 구독
+TOPIC_ARN="arn:aws:sns:${REGION}:000000000000:test-topic"
+QUEUE_ARN="arn:aws:sqs:${REGION}:000000000000:test-queue"
+awslocal sns subscribe \
+  --topic-arn "$TOPIC_ARN" \
+  --protocol sqs \
+  --notification-endpoint "$QUEUE_ARN" \
+  --region $REGION
+
+# SES
+awslocal ses verify-email-identity --email-address test@test.com --region $REGION
+
+echo "init localstack done"
 
